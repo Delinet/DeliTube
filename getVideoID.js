51b6b887ca43fcb58d8baab13f35bc95e5b8d44b
Delinet <candela.antonio@gmail.com>
2013-12-09 21:05:55 +0100

    Update getVideoID.js

--- getVideoID.js
+++ getVideoID.js
@@ -7,19 +7,37 @@
 
 
 getVideoId = function(video_url){
-			idx = video_url.indexOf('v=');
-			if (idx != -1 ){
-  				video_id = video_url.substring(idx+2);	
-			}
-			else{
-  				idx = video_url.indexOf('youtu.be/');
-  				if (idx != -1 ){
-    			  	video_id = video_url.substring(idx+9);
-  				}
-			}
-			idx = video_id.indexOf('&');
-			if (idx != -1){
-  				video_id = video_id.substring(0,idx);
-			}
-			return video_id
-	   };
+				idx = video_url.indexOf('v=');
+				if (idx != -1 ){
+  					video_id = video_url.substring(idx+2);	
+				}
+				else{
+  					idx = video_url.indexOf('youtu.be/');
+  					if (idx != -1 ){
+    			  		video_id = video_url.substring(idx+9);
+  					}
+  					else{
+  						idx = video_url.indexOf('v/');
+  				  		if (idx != -1){
+  				      		video_id = video_url.substring(idx+2);
+  				  		}
+  				  		else{
+  				     		idx = video_url.indexOf('embed/');
+  				     		if (idx != -1){
+  				       		video_id = video_url.substring(idx+6);
+  				     	}
+  				  	}
+  					}
+				}
+				idx = video_id.indexOf('&');
+				if (idx != -1){
+  					video_id = video_id.substring(0,idx);
+				}
+				else{
+			 		idx = video_id.indexOf('?');
+			 		if (idx != -1){
+  			 		video_id = video_id.substring(0,idx);
+  			 	}	
+				}
+				return video_id
+	   		};
